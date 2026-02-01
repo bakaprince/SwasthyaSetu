cbb610ca77da09cbec30784a8425ccfd93036f14
bakaprince <bakaprince.dev@gmail.com>
2026-02-01 20:45:36 +0530

    password remember me feature

--- js/config/app-config.js
+++ js/config/app-config.js
@@ -70,7 +70,10 @@ const AppConfig = {
         userProfile: 'swasthyasetu_user_profile',
         userLocation: 'swasthyasetu_user_location',
         theme: 'swasthyasetu_theme',
-        language: 'swasthyasetu_language'
+        language: 'swasthyasetu_language',
+        rememberMe: 'swasthyasetu_remember_me',
+        rememberedIdentifier: 'swasthyasetu_remembered_identifier',
+        rememberedUserType: 'swasthyasetu_remembered_user_type'
     },
 
     // Validation Rules
